72bb5e7c802349123641c4454182808af9a01786
Vishu Goyal <69478693+vishugoyal3@users.noreply.github.com>
2022-12-10 15:48:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 
 
 
-
-
 document.getElementById("alice").addEventListener("click",book);
 
 document.getElementById("hyde").addEventListener("click",book);
 
 document.getElementById("lord").addEventListener("click",book);
-
 
 
 function book(e){
@@ -19,8 +16,8 @@ function book(e){
     
         read("./books/AliceInWonderland.txt","Alice In WonderLand");
         
-        
         document.getElementById(m);
+
         m.innerHTML=`alice:385 time(s) <br>
         
         very:144 time(s) <br>
@@ -34,19 +31,18 @@ function book(e){
         
         
         l.innerHTML=`gather:1 time(s) <br>
-        
-        
         sorrows:1 time(s) <br>
+        
         joys: 1 time(s) <br>
-
-remembering: 1 time(s) <br>
-
-chile-life:1 time(s)`;
-
-    }
+        
+        remembering: 1 time(s) <br>
     
+    chile-life:1 time(s)`;
+    
+    }
+  
     if(e.target.id=="hyde"){
-
+    
         read("./books/JekyllAndHyde.txt","Jekyll And Hyde");
         
         document.getElementById(m);
@@ -56,11 +52,11 @@ chile-life:1 time(s)`;
         Hyde:284 time(s) <br>
         
         most:124 time(s) <br>
-        
         doctor:113 time(s) <br>
         
         its:101 time(s)`;
 
+        
         
         l.innerHTML=`job:1 time(s) <br>
         
@@ -85,8 +81,8 @@ chile-life:1 time(s)`;
         rings:149 time(s) <br>
         
         power:127 time(s) <br>
-        most:113 time(s) <br>
         
+        most:113 time(s) <br>
         
         for:101 time(s)`;
 
@@ -94,7 +90,6 @@ chile-life:1 time(s)`;
         l.innerHTML=`hope:1 time(s) <br>
         
         mercy:1 time(s) <br>
-        
         forever: 1 time(s) <br>
         
         together: 1 time(s) <br>
@@ -103,21 +98,22 @@ chile-life:1 time(s)`;
         
         
     }
+    
 }
 
 
 
+
 function read(url,title){
-
     var client = new XMLHttpRequest();
-client.open('GET', url);
 
-    client.onreadystatechange = function() {
-
-    var t=client.responseText;
-  document.getElementById("bhead").innerText=title;
+    client.open('GET', url);
+client.onreadystatechange = function() {
   
-        document.getElementById("btext").innerText=t;
+    var t=client.responseText;
+
+    document.getElementById("bhead").innerText=title;
+  document.getElementById("btext").innerText=t;
 
   details(t);
 }
@@ -126,24 +122,22 @@ client.open('GET', url);
 }
 
 
-
-
-
-
 const removeWords = [ "a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've" ];
 
-
-
 function details(t){
-    var lines=t.split("\n");
 
+    var lines=t.split("\n");
+    
     var words=[];
     
     var wf=[];
     
-    lines.forEach((line)=>{
-        let ww=line.split(" ");
     
+    
+    lines.forEach((line)=>{
+    
+        let ww=line.split(" ");
+        
         ww.forEach((w)=>{
 
             
@@ -151,19 +145,20 @@ function details(t){
             if(removeWords.indexOf(w)==-1 && !(w==" "))
             
             {
-                words.push(w);
             
+                words.push(w);
                 wf.push(t.split(w).length);
             }
             
+            
         })
+        
     })
- 
     // console.log(words);
-    
+ 
     let max=0;
-    
     let w1="";
+    
     for(let i=0;i<words.length;i++){
     
         if(wf[i]>max){
@@ -175,10 +170,7 @@ function details(t){
         
     }
     console.log(w1);
-
+    (document.getElementById("stats")).innerHTML=`Document Length: ${t.length} <br> Words Count: ${words.length}`;
 
 
 }
-
-
-
